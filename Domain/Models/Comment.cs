d74cc3345c5fdb6f53d1130282e94186655072c6
M0Rek <morek.090@gmail.com>
2022-11-22 21:10:59 +0100

    Remove unused variables.

--- Domain/Models/Comment.cs
+++ Domain/Models/Comment.cs
@@ -7,6 +7,4 @@ public class Comment
     public int CommentedOnId { get; set; }
     public string Content { get; set; }
     public DateTime PublishedDateTime { get; set; }
-    public List<int> UpVotedBy { get; set; } = new();
-    public List<int> DownVotedBy { get; set; } = new();
 }
--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -7,8 +7,6 @@ public class Post
     public string Title { get; set; }
     public string Content { get; set; }
     public DateTime PublishedDateTime { get; set; }
-    public List<int> UpVotedBy { get; set; } = new();
-    public List<int> DownVotedBy { get; set; } = new();
     
     
 }

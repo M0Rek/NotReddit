12188d9e558c817a0397654a0a07d086b5db268e
M0Rek <morek.090@gmail.com>
2022-11-23 12:35:30 +0100

    Implement PostEfcDao.cs

--- EfcDataAccess/DAOs/PostEfcDao.cs
+++ EfcDataAccess/DAOs/PostEfcDao.cs
@@ -1,22 +1,36 @@
 using Application.IDAOs;
 using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace EfcDataAccess.DAOs;
 
 public class PostEfcDao : IPostDao
 {
-    public Task<Post> CreateAsync(Post post)
+    private readonly NotRedditContext _context;
+
+    public PostEfcDao(NotRedditContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task<IEnumerable<Post>> GetAsync()
+    public async Task<Post> CreateAsync(Post post)
     {
-        throw new NotImplementedException();
+        post.PublishedDateTime = DateTime.Now;
+
+        EntityEntry<Post> newPost = await _context.Posts.AddAsync(post);
+
+        await _context.SaveChangesAsync();
+        return newPost.Entity;
     }
 
-    public Task<Post?> GetByIdAsync(int id)
+    public async Task<IEnumerable<Post>> GetAsync()
     {
-        throw new NotImplementedException();
+        return await _context.Posts.ToListAsync();
+    }
+
+    public async Task<Post?> GetByIdAsync(int id)
+    {
+        return await _context.Posts.Include(post => post.OriginalPoster).FirstOrDefaultAsync(post => post.Id == id);
     }
 }

d384e9117cb5c999d202e22eebd773c57c5dc35a
M0Rek <morek.090@gmail.com>
2022-11-23 12:35:39 +0100

    Implement CommentEfcDao.cs

--- EfcDataAccess/DAOs/CommentEfcDao.cs
+++ EfcDataAccess/DAOs/CommentEfcDao.cs
@@ -1,17 +1,31 @@
 using Application.IDAOs;
 using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace EfcDataAccess.DAOs;
 
 public class CommentEfcDao : ICommentDao
 {
-    public Task<Comment> CreateAsync(Comment comment)
+    private readonly NotRedditContext _context;
+
+    public CommentEfcDao(NotRedditContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task<IEnumerable<Comment>> GetByPostIdAsync(int id)
+    public async Task<Comment> CreateAsync(Comment comment)
     {
-        throw new NotImplementedException();
+        comment.PublishedDateTime = DateTime.Now;
+        
+        EntityEntry<Comment> newComment = await _context.Comments.AddAsync(comment);
+        
+        await _context.SaveChangesAsync();
+        return newComment.Entity;
+    }
+
+    public async Task<IEnumerable<Comment>> GetByPostIdAsync(int id)
+    {
+        return await _context.Comments.Where(comment => comment.CommentedOn.Id == id).Include(comment => comment.OriginalPoster).ToListAsync();
     }
 }
